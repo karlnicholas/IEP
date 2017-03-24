8c579a2e6d10838c1c6afb7d9bc88ab51e57996f
karlnicholas <karl.nicholas@outlook.com>
2017-03-24 11:39:55 -0700

    Updates new API

---
+++ src/main/java/quote/GetQuote.java
@@ -0,0 +1,46 @@
+package quote;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.Connection.Response;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import sep.lucene.SearchResult;
+
+public class GetQuote {
+	protected static final String URL_PREFIX = "http://plato.stanford.edu/cgi-bin/encyclopedia/random";
+	public SearchResult getEntryFromStanford() {
+		String text = "";
+		String subject = "";
+		String url = "";
+		Document doc;
+		try {
+			Response response = Jsoup.connect(URL_PREFIX)
+	                .followRedirects(true) //to follow redirects
+	                .execute();
+			doc = response.parse();
+			Elements preamble = doc.select("div[id=preamble] p");
+			URL rUrl = response.url();
+			if ( rUrl != null )
+				url = rUrl.toString();
+			
+			Elements subjects = doc.select("div[id=aueditable] h1");
+			if ( subjects != null ) 
+				if ( subjects.first() != null )
+					subject = subjects.first().ownText();
+			
+			if (preamble != null) {
+				if (preamble.first() != null)
+					text = preamble.first().text();
+			}
+		} catch (IOException e) {
+//			log.error(e.getMessage());
+			text = e.getLocalizedMessage();
+		}
+		return new SearchResult(subject, url, subject, text, (float)1.0);
+	}
+
+}
--- src/main/java/sep/DoSearch.java
+++ src/main/java/sep/DoSearch.java
@@ -14,7 +14,7 @@ public class DoSearch {
 	}
 	public void doSearch() throws ParseException, IOException {
 		SearchFiles searchFiles = new SearchFiles();
-		List<SearchResult> searchResults = searchFiles.query("cynicism");
+		List<SearchResult> searchResults = searchFiles.query("chomsky");
 		if ( searchResults.size() > 0 ) {
 			System.out.println("Found results: " + searchResults.size());
 			for ( SearchResult searchResult: searchResults ) {
